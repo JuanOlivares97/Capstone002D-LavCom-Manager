dd49aba76fe6deaaad882f2d9928799e80794ca3
Juan Manuel Olivares Jiménez <jua.olivares@duocuc.cl>
2024-09-16 19:39:48 -0300

    add express-ejs-layout
    Se agrega libreria para renderizar layouts

--- laundry-manager/src/app.js
+++ laundry-manager/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const expressLayout = require('express-ejs-layouts')
 const path = require('path');
 const userRouter = require('./routes/user.routes');
 const articulosRouter = require('./routes/articulos.routes');
 
 const app = express();
+
+app.use(expressLayout)
+app.set('layout', '_template')
 
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
@@ -12,10 +16,10 @@ app.use(express.static(path.join(__dirname, "static")))
 console.log(path.join(__dirname, "static"));
 
 app.use('/', userRouter);
-app.use('/', articulosRouter);
+app.use('/clothes', articulosRouter);
 
 app.get("/template", (req, res) => {
-    res.render("articulos");
+    res.render("clothes/articulos");
 });
 
 const port = process.env.PORT || 3000;
