35fb218d8475d54eb9062a70a4731deda2d00da4
ignacio diaz <ign.diaz@duocuc.cl>
2024-09-16 12:31:19 -0300

    controllers for users and articles

---
+++ laundry-manager/src/controllers/articulos.controller.js
@@ -0,0 +1,252 @@
+const articulos = [
+    {
+        "id_articulo": "1",
+        "nombre_articulo": "Camisa paciente",
+        "stock": "95",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "2",
+        "nombre_articulo": "cubre cama grande",
+        "stock": "108",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "3",
+        "nombre_articulo": "cubre cama chico",
+        "stock": "94",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "4",
+        "nombre_articulo": "Delantal verde",
+        "stock": "84",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "5",
+        "nombre_articulo": "Frazada grande",
+        "stock": "91",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "6",
+        "nombre_articulo": "frazada chica",
+        "stock": "102",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "7",
+        "nombre_articulo": "Pantalon verde",
+        "stock": "90",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "8",
+        "nombre_articulo": "Pantalon celeste",
+        "stock": "105",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "9",
+        "nombre_articulo": "Pechera verde",
+        "stock": "95",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "10",
+        "nombre_articulo": "Pechera Celeste",
+        "stock": "110",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "11",
+        "nombre_articulo": "Pa\u00f1o perforado",
+        "stock": "108",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "12",
+        "nombre_articulo": "Pa\u00f1o clinico",
+        "stock": "117",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "13",
+        "nombre_articulo": "Sabana",
+        "stock": "102",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "14",
+        "nombre_articulo": "Sabana clinica",
+        "stock": "107",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "15",
+        "nombre_articulo": "Sabana perforada",
+        "stock": "112",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "16",
+        "nombre_articulo": "Sabana cuna",
+        "stock": "106",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "17",
+        "nombre_articulo": "Toalla chica",
+        "stock": "130",
+        "id_subgrupo_ropa": "3",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "18",
+        "nombre_articulo": "Toalla grande",
+        "stock": "115",
+        "id_subgrupo_ropa": "3",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "19",
+        "nombre_articulo": "Sabanilla",
+        "stock": "110",
+        "id_subgrupo_ropa": "3",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "20",
+        "nombre_articulo": "Contenciones",
+        "stock": "101",
+        "id_subgrupo_ropa": "3",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "21",
+        "nombre_articulo": "Cortina",
+        "stock": "115",
+        "id_subgrupo_ropa": "3",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "22",
+        "nombre_articulo": "Funda almohada",
+        "stock": "117",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "23",
+        "nombre_articulo": "Hule",
+        "stock": "109",
+        "id_subgrupo_ropa": "3",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "24",
+        "nombre_articulo": "Crea",
+        "stock": "116",
+        "id_subgrupo_ropa": "3",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "25",
+        "nombre_articulo": "Fajas",
+        "stock": "110",
+        "id_subgrupo_ropa": "3",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "26",
+        "nombre_articulo": "Plumon",
+        "stock": "114",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "27",
+        "nombre_articulo": "Alforja",
+        "stock": "114",
+        "id_subgrupo_ropa": "3",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "28",
+        "nombre_articulo": "Cubrecamilla",
+        "stock": "104",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "29",
+        "nombre_articulo": "Almohada",
+        "stock": "100",
+        "id_subgrupo_ropa": "1",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "30",
+        "nombre_articulo": "Picaron",
+        "stock": "102",
+        "id_subgrupo_ropa": "3",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "31",
+        "nombre_articulo": "Sabanilla Verde",
+        "stock": "95",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "32",
+        "nombre_articulo": " guantes",
+        "stock": "688",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "33",
+        "nombre_articulo": " mascarilla",
+        "stock": "200",
+        "id_subgrupo_ropa": "2",
+        "borrado": "0"
+    },
+    {
+        "id_articulo": "34",
+        "nombre_articulo": " Bata",
+        "stock": "200",
+        "id_subgrupo_ropa": "3",
+        "borrado": "1"
+    }
+]
+
+function getArticulos(req, res) {
+    try {
+        return res.status(200).json(articulos);
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+module.exports = {
+    getArticulos
+}
---
+++ laundry-manager/src/controllers/user.controller.js
@@ -0,0 +1,29 @@
+function renderLogin(req, res) {
+    try {
+        return res.render("login");
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+function renderRecuperarContrasenaForm(req, res) {
+    try {
+        return res.render("recuperar_pwd_form");
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+function renderRecuperarContrasenaInfo(req, res) {
+    try {
+        return res.render("recuperar_pwd_info");
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+module.exports = {
+    renderLogin,
+    renderRecuperarContrasenaForm,
+    renderRecuperarContrasenaInfo
+}
