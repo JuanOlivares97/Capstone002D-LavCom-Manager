ef139c9f57f511d93e226deb0933541d94bc0db7
CataLazo <cat.lazo@duocuc.cl>
2024-09-25 00:04:06 -0300

    changes dropdown sidebar

--- laundry-manager/src/static/js/colapsar_menu.js
+++ laundry-manager/src/static/js/colapsar_menu.js
@@ -1,17 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleSidebarButton = document.getElementById('toggleSidebar');
+    const sidebar = document.getElementById('sidebar');
 
-        // Seleccionamos el botón y el sidebar
-        const toggleButton = document.getElementById('toggleSidebar');
-        const sidebar = document.getElementById('sidebar');
-        const menuItems = document.querySelectorAll('#menu li span'); // Para ocultar los textos
-
-        // Añadimos el evento de clic para alternar la clase
-        toggleButton.addEventListener('click', function() {
-            sidebar.classList.toggle('collapsed');
-            
-            // Ajustamos la visibilidad de los textos
-            if (sidebar.classList.contains('collapsed')) {
-                menuItems.forEach(item => item.style.display = 'none');
-            } else {
-                menuItems.forEach(item => item.style.display = 'inline');
-            }
-        });
+    toggleSidebarButton.addEventListener('click', () => {
+        sidebar.classList.toggle('collapsed');
+    });
+});
