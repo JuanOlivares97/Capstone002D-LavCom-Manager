0b9d481aa0dfdd5542e41843767095a5657ca6e8
ignacio diaz <ign.diaz@duocuc.cl>
2024-09-26 00:05:35 -0300

    config for forms and json for requests body

--- laundry-manager/src/app.js
+++ laundry-manager/src/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const expressLayout = require('express-ejs-layouts')
 const path = require('path');
-
 const userRouter = require('./routes/user.routes');
 const authRouter = require('./routes/auth.routes');
 const articulosRouter = require('./routes/articulos.routes');
 const reportesRouter = require('./routes/reportes.routes')
 const dashboardRouter = require('./routes/dashboard.routes')
 const helpRouter = require('./routes/help.routes')
+const cookieParser = require('cookie-parser');
+
 const app = express();
+
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 app.use(expressLayout)
 app.set('layout', '_template')
