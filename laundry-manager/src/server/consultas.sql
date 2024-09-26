ca01ab9ef57bc65904df73f22b9a583d4c5c0e8c
ignacio diaz <ign.diaz@duocuc.cl>
2024-09-26 09:35:24 -0300

    deleted useless file

--- laundry-manager/src/server/consultas.sql
+++
@@ -1,9 +0,0 @@
---nombres de columnas
-SELECT GROUP_CONCAT(COLUMN_NAME)
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'tabla'
-AND TABLE_SCHEMA = 'nombre_base_datos';
-
--- necesito un select * from que retorne todo en json
-SELECT CONCAT('[', GROUP_CONCAT(JSON_OBJECT('nombre_columna', nombre_columna)), ']')
-FROM tabla;
