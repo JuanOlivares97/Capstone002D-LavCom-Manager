155207cd61c3f5fc8eb85430453b4ca92e60ed13
ignacio diaz <ign.diaz@duocuc.cl>
2024-09-27 22:42:37 -0300

    calcelled and lost clothes

--- laundry-manager/src/controllers/reportes.controller.js
+++ laundry-manager/src/controllers/reportes.controller.js
@@ -11,7 +11,7 @@ function renderHome(req, res) {
 
 async function getFullReport(req, res) {
     try {
-        const result = await prisma.$queryRaw`CALL ropa_disponible_hospital()`;
+        const result = await prisma.$queryRaw`CALL ropa_disponible_hospital();`;
 
         // Format the result to map the column names correctly
         const formattedResult = result.map(row => ({
@@ -53,6 +53,28 @@ async function getServicesReport(req, res) {
         
         return res.status(200).json(result);
     } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+async function getBajasyPerdidas(req, res) {
+    try {
+        const result = await prisma.$queryRaw`CALL bajas_perdidas();`;
+
+        // Format the result
+        const formattedResult = result.map(row => ({
+            id_articulo: row.f0,
+            nombre_articulo: row.f1,
+            perdidas_externas: row.f2,
+            perdidas_internas: row.f3,
+            perdidas_totales: row.f4,
+            bajas_servicio: row.f5,
+            bajas_roperia: row.f6,
+            bajas_totales: row.f7
+        }));
+
+        return res.status(200).json(formattedResult);
+    } catch (error) {
         return res.status(500).json({ message: "Internal server error", error });
     }
 }
@@ -60,5 +82,6 @@ async function getServicesReport(req, res) {
 module.exports = {
     renderHome,
     getFullReport,
-    getServicesReport
+    getServicesReport,
+    getBajasyPerdidas
 };
