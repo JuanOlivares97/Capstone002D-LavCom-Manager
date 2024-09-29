2ce66cb49077dad91a320ed0dc684818490655f3
Juan Manuel Olivares Jiménez <101352442+JuanOlivares97@users.noreply.github.com>
2024-09-29 03:16:42 -0300

    Create employee.routes.js

---
+++ food-manager/src/routes/employee.routes.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const employeeController = require('../controllers/employee.controller.js')
+const router = Router();
+
+router.get('/home', employeeController.renderHome)
+
+module.exports = router;
