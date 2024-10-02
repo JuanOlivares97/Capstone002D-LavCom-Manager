a595da2251ff32ecb17a06a40a129d11d58a5b84
Natalia Godoy <Na.godoy@duocuc.cl>
2024-10-02 18:09:21 -0300

    funciones del CRUD de articulos
    Se realiza la función del create, delete y update de los articulos.

--- laundry-manager/src/controllers/articulos.controller.js
+++ laundry-manager/src/controllers/articulos.controller.js
@@ -1,14 +1,14 @@
-const prisma = require('../server/prisma');
+const prisma = require("../server/prisma");
 
 async function renderHome(req, res) {
     try {
         const usuarios = await prisma.usuarios.findMany({
             where: {
-                borrado: false
-            }
+                borrado: false,
+            },
         });
         const servicios = await prisma.servicio.findMany();
-        res.render('clothes/home', { usuarios, servicios });
+        res.render("clothes/home", { usuarios, servicios });
     } catch (error) {
         return res.status(500).json({ message: "Internal server error" });
     }
@@ -18,11 +18,11 @@ async function getArticulos(req, res) {
     try {
         const articulos = await prisma.articulo.findMany({
             where: {
-                borrado: false
+                borrado: false,
             },
             include: {
-                subgrupo_ropa: true
-            }
+                subgrupo_ropa: true,
+            },
         });
         return res.status(200).json(articulos);
     } catch (error) {
@@ -30,7 +30,85 @@ async function getArticulos(req, res) {
     }
 }
 
+async function createArticulo(req, res) {
+    try {
+        const newArticulo = {
+            nombre_articulo: req.body.nombre,
+            stock: parseInt(req.body.stock),
+            id_subgrupo_ropa: parseInt(req.body.subgrupo),
+            borrado: false,
+        };
+        const articulo = await prisma.articulo.create({
+            data: newArticulo,
+        });
+        if (!articulo) {
+            return res.status(400).json({ message: "Error creating user" });
+        }
+        return res
+            .status(201)
+            .json({ message: "Articulo creado exitosamente" });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Internal server  error  " + error });
+    }
+}
+
+async function updateArticulo(req, res) {
+    try {
+        const articulo = {
+            nombre_articulo: req.body.nombre,
+            stock: parseInt(req.body.stock),
+            id_subgrupo_ropa: parseInt(req.body.subgrupo),
+            borrado: false,
+        };
+        const articulos = await prisma.articulo.update({
+            where: {
+                id_articulo: parseInt(req.body.id_articulo),
+            },
+            data: articulo,
+        });
+        if (!articulos) {
+            return res.status(400).json({ message: "Error updating user" });
+        }
+
+        return res
+            .status(200)
+            .json({ message: "Usuario actualizado exitosamente" });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Internal server error  " + error });
+    }
+}
+
+async function deleteArticulo(req, res) {
+    try {
+        const articulo = await prisma.articulo.update({
+            where: {
+                id_articulo: parseInt(req.body.id_articulo),
+            },
+            data: {
+                borrado: true,
+            },
+        });
+        if (!articulo) {
+            return res.status(400).json({ message: "Error deleting user" });
+        }
+        return res
+            .status(200)
+            .json({ message: "Usuario eliminado exitosamente" });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Internal server error  " + error });
+    }
+}
+
 module.exports = {
     getArticulos,
-    renderHome
-}
+    renderHome,
+    createArticulo,
+    updateArticulo,
+    deleteArticulo,
+};
