5b209919a93ca23529e1fc5859943d3c7b93d3cd
ignacio diaz <ign.diaz@duocuc.cl>
2024-10-05 18:34:43 -0300

    route for cancelling clothes

--- laundry-manager/src/routes/articulos.routes.js
+++ laundry-manager/src/routes/articulos.routes.js
@@ -10,5 +10,6 @@ router.put("/update-clothes", articulosController.updateArticulo);
 router.delete("/delete-clothes", articulosController.deleteArticulo);
 
 router.post("/entregar-unidad-sigcom", articulosController.entregarUnidadSigcom);
+router.post("/dar-ropa-baja", articulosController.darRopaDeBaja);
 
 module.exports = router;
