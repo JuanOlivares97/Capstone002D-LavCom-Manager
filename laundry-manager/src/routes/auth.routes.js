dc1ee7b655d4f9d673179e285e2638e644fc3c8b
ignacio diaz <ign.diaz@duocuc.cl>
2024-10-15 13:14:23 -0300

    route to set email on first time login

--- laundry-manager/src/routes/auth.routes.js
+++ laundry-manager/src/routes/auth.routes.js
@@ -7,6 +7,7 @@ router.get('/login', authController.renderLogin);
 router.get('/recuperar_pwd_form', authController.renderRecuperarContrasenaForm);
 router.get('/recuperar_pwd_info', authController.renderRecuperarContrasenaInfo);
 router.post('/login', authController.login);
+router.put('/set-email', authController.setEmail);
 
 router.post('/logout', authController.logout);
 
