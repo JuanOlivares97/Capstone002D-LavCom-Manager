9cc5d7f739ab6a8dbc6af6075a1dbc96f3b3dfad
Juan Manuel Olivares Jiménez <Na.godoy@duocuc.cl>
2024-10-16 16:47:09 -0300

    add enpoint to get employees

--- food-manager/src/routes/employee.routes.js
+++ food-manager/src/routes/employee.routes.js
@@ -3,6 +3,6 @@ const employeeController = require('../controllers/employee.controller.js')
 const router = Router();
 
 router.get('/home', employeeController.renderHome)
-
+router.get('/get-funcionarios', employeeController.getFuncionarios)
 
 module.exports = router;
