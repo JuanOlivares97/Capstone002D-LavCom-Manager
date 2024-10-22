a46f5d9ee2f9d7a542e261d5589948f4aba44c92
Juan Manuel Olivares Jiménez <Na.godoy@duocuc.cl>
2024-10-21 23:21:05 -0300

    Update employee.controller.js

--- food-manager/src/controllers/employee.controller.js
+++ food-manager/src/controllers/employee.controller.js
@@ -1,8 +1,21 @@
 const prisma = require('../server/prisma');
+const {
+    getContrato,
+    getServicio,
+    getUnidad,
+    getEstamento,
+    getTipoFuncionario } = require('./maintainer.controller');
 
 async function renderHome(req, res) {
     try {
-        res.render('employee/home', { tipoUsuario: 1 }); 
+        const contrato = await getContrato();
+        const servicios = await getServicio();
+        const unidades = await getUnidad();
+        const estamentos = await getEstamento();
+        const tipoFuncionario = await getTipoFuncionario();
+        
+        res.render('employee/home', { tipoUsuario: 1, contrato, servicios, unidades, estamentos, tipoFuncionario });
+
     } catch (error) {
         return res.status(500).json({ message: "Internal server error" });
     }
@@ -37,6 +50,88 @@ async function getFuncionarios(req, res) {
         res.status(500).json({ message: "Internal server error: " + error.message });
     }
 }
+
+async function createEmployee(req, res){
+    try {
+        const { nombre_usuario, rut_usuario, dv_usuario, correo, telefono, direccion, tipoEstamento, tipoServicio, tipoUnidad, tipoContrato } = req.body;
+        const funcionario = await prisma.funcionario.create({
+            data: {
+                NombreFuncionario: nombre_usuario,
+                RutFuncionario: rut_usuario,
+                DvFuncionario: dv_usuario,
+                CorreoFuncionario: correo,
+                TelefonoFuncionario: telefono,
+                DireccionFuncionario: direccion,
+                TipoEstamento: {
+                    connect: {
+                        id: tipoEstamento
+                    }
+                },
+                TipoServicio: {
+                    connect: {
+                        id: tipoServicio
+                    }
+                },
+                TipoUnidad: {
+                    connect: {
+                        id: tipoUnidad
+                    }
+                },
+                TipoContrato: {
+                    connect: {
+                        id: tipoContrato
+                    }
+                }
+            },
+        });
+        res.status(201).json(funcionario);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error: " + error.message });
+    }
+}
+
+async function updateEmployee(req, res){
+    try {
+        const { id, nombre_usuario, rut_usuario, dv_usuario, correo, telefono, direccion, tipoEstamento, tipoServicio, tipoUnidad, tipoContrato } = req.body;
+        const funcionario = await prisma.funcionario.update({
+            where: {
+                IdFuncionario: id
+            },
+            data: {
+                NombreFuncionario: nombre_usuario,
+                RutFuncionario: rut_usuario,
+                DvFuncionario: dv_usuario,
+                CorreoFuncionario: correo,
+                TelefonoFuncionario: telefono,
+                DireccionFuncionario: direccion,
+                TipoEstamento: {
+                    connect: {
+                        id: tipoEstamento
+                    }
+                },
+                TipoServicio: {
+                    connect: {
+                        id: tipoServicio
+                    }
+                },
+                TipoUnidad: {
+                    connect: {
+                        id: tipoUnidad
+                    }
+                },
+                TipoContrato: {
+                    connect: {
+                        id: tipoContrato
+                    }
+                }
+            },
+        });
+        res.status(200).json(funcionario);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error: " + error.message });
+    }
+}
+
 function capitalizeWords(str) {
     return str
         .toLowerCase()
@@ -48,5 +143,7 @@ function capitalizeWords(str) {
 
 module.exports = {
     renderHome,
-    getFuncionarios
+    getFuncionarios,
+    createEmployee,
+    updateEmployee
 }
