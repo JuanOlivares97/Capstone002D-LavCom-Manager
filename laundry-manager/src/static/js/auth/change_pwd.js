4bb01ebd5ab5577fcdc692d802e58ec7e55def36
ignacio diaz <ign.diaz@duocuc.cl>
2024-10-22 21:39:13 -0300

    recover pwd frontend validations

--- laundry-manager/src/static/js/auth/change_pwd.js
+++ laundry-manager/src/static/js/auth/change_pwd.js
@@ -18,7 +18,7 @@ document.querySelectorAll('#form_nueva_pwd input[type="text"]').forEach((input, 
     });
 });
 
-document.getElementById('form_nueva_pwd').addEventListener('submit', function(event) {
+document.getElementById('form_nueva_pwd').addEventListener('submit', async function(event) {
     event.preventDefault();
     const inputs = document.querySelectorAll('#form_nueva_pwd input[type="text"]');
     let code = '';
@@ -27,5 +27,50 @@ document.getElementById('form_nueva_pwd').addEventListener('submit', function(ev
     });
     const pwd1 = document.querySelector('input[name="pwd1"]').value;
     const pwd2 = document.querySelector('input[name="pwd2"]').value;
-    alert(`Código: ${code}\nContraseña: ${pwd1}\nConfirmar contraseña: ${pwd2}`);
+    if (pwd1 !== pwd2) {
+        Swal.fire({
+            icon: "error",
+            title: "Las contraseñas no coinciden, intenta de nuevo",
+            timer: 2000, // La alerta se mostrará durante 3 segundos
+            timerProgressBar: true,
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false, // No mostrar botón de confirmación
+        });
+        return;
+    }
+    await fetch("/auth/change-pwd", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ code, pwd: pwd1 })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            Swal.fire({
+                icon: "success",
+                title: data.message,
+                timer: 2000,
+                timerProgressBar: true,
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false
+            });
+            setTimeout(() => {
+                window.location.href = "/auth/login";
+            }, 2000);
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: data.message,
+                timer: 2000,
+                timerProgressBar: true,
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false
+            });
+        }
+    })
 });
