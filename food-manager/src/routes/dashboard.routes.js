1e517c7faafcaaa34ba884ebaeb599553f4dd56e
CataLazo <101206733+CataLazo@users.noreply.github.com>
2024-10-28 23:23:14 -0300

    fix error

--- food-manager/src/routes/dashboard.routes.js
+++ food-manager/src/routes/dashboard.routes.js
@@ -2,6 +2,6 @@ const { Router } = require('express');
 const dashboardController = require('../controllers/dashboard.controller.js')
 const router = Router();
 
-router.get('/home', dashboardController.renderHome)
+router.get('/home', dashboardController.renderDashboard)
 
 module.exports = router;
