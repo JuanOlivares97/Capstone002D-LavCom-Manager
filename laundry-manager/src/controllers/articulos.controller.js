855617bcbf81784fb7638d0a7028d4ae868d7072
ignacio diaz <ign.diaz@duocuc.cl>
2024-10-31 18:31:57 -0300

    more return fixing

--- laundry-manager/src/controllers/articulos.controller.js
+++ laundry-manager/src/controllers/articulos.controller.js
@@ -81,12 +81,12 @@ async function updateArticulo(req, res) {
             data: articulo,
         });
         if (!articulos) {
-            return res.status(400).json({ message: "Error updating user" });
+            return res.status(400).json({ message: "Error al actualizar artículo", success: false });
         }
 
-        return res.status(200).json({ message: "Usuario actualizado exitosamente" });
+        return res.status(200).json({ message: "Artículo actualizado exitosamente", success: true });
     } catch (error) {
-        return res.status(500).json({ message: "Internal server error  " + error });
+        return res.status(500).json({ message: "Internal server error", error, success: false });
     }
 }
 
