c17c5a007e09e5f212f331ea0e7b0ce0c3c688f4
CataLazo <101206733+CataLazo@users.noreply.github.com>
2024-10-30 21:54:23 -0300

    Update maintainer.routes.js

--- food-manager/src/routes/maintainer.routes.js
+++ food-manager/src/routes/maintainer.routes.js
@@ -4,4 +4,13 @@ const router = Router();
 
 router.get('/home', maintainerController.renderHome)
 
+router.post('/create-regimen', maintainerController.createTipoRegimen);
+router.post('/create-servicio', maintainerController.createService);
+router.post('/create-estamento', maintainerController.createEstamento);
+router.post('/create-unidad', maintainerController.createUnidad);
+router.post('/create-via', maintainerController.createVia);
+router.post('/create-tipo-funcionario', maintainerController.createTipoFuncionario);
+router.post('/create-contrato', maintainerController.createContrato);
+
+
 module.exports = router;
