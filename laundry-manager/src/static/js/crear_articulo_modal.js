2a6d76d5f47397dd6080f86ce2c67bfa6d7e8613
ignacio diaz <ign.diaz@duocuc.cl>
2024-10-31 22:29:13 -0300

    connect front and back for clothing items creation

--- laundry-manager/src/static/js/crear_articulo_modal.js
+++ laundry-manager/src/static/js/crear_articulo_modal.js
@@ -18,4 +18,64 @@ document.addEventListener("DOMContentLoaded", function() {
             modal.classList.add('hidden');
         }, 300); // Duration should match the CSS transition duration
     });
+
+    modal.addEventListener("submit", async function(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const newClothingItem = Object.fromEntries(formData);
+        
+        // Asegúrate que los nombres coincidan con lo que espera el backend
+        const dataToSend = {
+            nombre: newClothingItem.nombre,
+            stock: parseInt(newClothingItem.stock),
+            subgrupo: parseInt(newClothingItem.subgrupo)
+        };
+        
+        try {
+            const response = await fetch("/clothes/create-clothes", {
+                method: "POST",
+                body: JSON.stringify(dataToSend),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+    
+            const data = await response.json();
+            
+            if (!data.success) {
+                Swal.fire({
+                    title: "Error",
+                    text: data.message,
+                    icon: "error",
+                });
+                return;
+            }
+    
+            Swal.fire({
+                title: "Éxito",
+                text: data.message,
+                icon: "success"
+            }).then(() => {
+                modal.classList.add('hidden');
+                
+                // Asegúrate que los campos coincidan con las columnas de ag-grid
+                const gridData = {
+                    id_articulo: data.articulo.id_articulo,
+                    nombre_articulo: data.articulo.nombre_articulo,
+                    stock: data.articulo.stock,
+                    subgrupo: data.articulo.subgrupo_ropa.desc_subgrupo,
+                    id_subgrupo_ropa: data.articulo.id_subgrupo_ropa    
+                };
+                
+                window.gridApi.applyTransaction({ add: [gridData] });
+                e.target.reset();
+            });
+        } catch (error) {
+            Swal.fire({
+                title: "Error",
+                text: "Hubo un error al crear el artículo",
+                icon: "error",
+            });
+        }
+    });
 });
