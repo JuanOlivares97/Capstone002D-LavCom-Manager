37ba3946855d2fcc315aa3fb413da3acb54b4e69
Juan Manuel Olivares JImenez <jua.olivares@duocuc.cl>
2024-11-04 03:17:40 -0300

    finish report routes, functions, and logic

--- food-manager/src/routes/report.routes.js
+++ food-manager/src/routes/report.routes.js
@@ -4,4 +4,8 @@ const router = Router();
 
 router.get('/home', reportController.renderHome)
 
+router.get('/fill-table', reportController.fillTable)
+
+router.get('/reporte-hospitalizados-diario', reportController.reportHospitalizadoDiario);
+
 module.exports = router;
