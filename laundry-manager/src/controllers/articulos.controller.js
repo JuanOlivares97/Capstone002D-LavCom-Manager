d2e9b3dda8fe464c27721de64ead7be22f6fd430
ignacio diaz <ign.diaz@duocuc.cl>
2024-11-04 22:22:56 -0300

    fix dar ropa de baja

--- laundry-manager/src/controllers/articulos.controller.js
+++ laundry-manager/src/controllers/articulos.controller.js
@@ -117,7 +117,6 @@ async function entregarUnidadSigcom(req, res) {
         var fecha = new Date();
         fecha = tempo.format(fecha, "YYYY-MM-DD HH:mm:ss A", "cl");
         const data = req.body;
-        console.log(data)
         const rut_usuario_1 = req.cookies["rutLogueado"];
         const result = await prisma.registro.create({
             data: {
@@ -152,11 +151,15 @@ async function darRopaDeBaja(req, res) {
     try {
         var fecha = new Date();
         fecha = tempo.format(fecha, "YYYY-MM-DD HH:mm:ss A", "cl");
-        const data = req.body;
+        let data = req.body;
+        const rut_usuario_1 = req.cookies["rutLogueado"];
+        if (data.tipo_dada_de_baja === "8") {
+            data.id_unidad_sigcom = null;
+        }
         const resultado = await prisma.$transaction(async (prisma) => {
             const registro = await prisma.registro.create({
                 data: {
-                    rut_usuario_1: parseInt(data.rut_usuario_1),
+                    rut_usuario_1: parseInt(rut_usuario_1),
                     id_tipo_registro: parseInt(data.tipo_dada_de_baja),
                     id_unidad_sigcom: data.id_unidad_sigcom ? parseInt(data.id_unidad_sigcom) : null,
                     observacion: data.observaciones ? data.observaciones.toString() : null,
@@ -188,10 +191,14 @@ async function darRopaDeBaja(req, res) {
             return registro;
         });
 
-        return res.status(200).json({ message: "Registro creado exitosamente", data: resultado });
+        if (!resultado) {
+            return res.status(400).json({ message: "Error creando registro", success: false });
+        }
+
+        return res.status(200).json({ message: "Registro creado exitosamente", success: true });
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ message: "Internal server error", error: error.message });
+        return res.status(500).json({ message: "Internal server error", error, success: false });
     }
 }
 
