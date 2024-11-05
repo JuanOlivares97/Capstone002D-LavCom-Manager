8e340282c89f7ed1d8a1e8f0c0c99bad366fa3d3
Juan Manuel Olivares JImenez <jua.olivares@duocuc.cl>
2024-11-04 23:34:23 -0300

    Update lunch.routes.js

--- food-manager/src/routes/lunch.routes.js
+++ food-manager/src/routes/lunch.routes.js
@@ -3,7 +3,10 @@ const lunchController = require('../controllers/lunch.controller.js')
 const router = Router();
 
 router.get('/home', lunchController.renderHome)
-
+router.get('/list', lunchController.renderLunchList);
+    
 router.post('/register-lunch', lunchController.registrationLunch);
+
+
 
 module.exports = router;
